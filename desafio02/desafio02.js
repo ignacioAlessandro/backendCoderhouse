bb1ce632aa08248797c717555b26a442a6f99acc
ias <alessandroignacio4@gmail.com>
2022-07-30 23:07:46 -0300

    desafio 2 comit final

--- desafio02/desafio02.js
+++ desafio02/desafio02.js
@@ -1,36 +1,76 @@
-const {promises: fs} = require('fs');
+
+const { promises: fs } = require('fs');
+const { arch } = require('os');
+
 
 class Contenedor {
     constructor(ruta){
-        this.ruta = rata;
+        this.ruta = ruta;
     }
 
+    
     async save(obj){
+        const products = await this.getAll();
+        let archivos = await products.archivos
+        const idArchivos = await archivos.map(x=>x.id)
+        const maxId = await Math.max(...idArchivos)
+        if (maxId>=1){
+            obj.id = await maxId+1; 
+            archivos.push(obj)
+            const arrayId =await archivos.map((x,i)=>x.arrayposition = i)
+            const nuevaLista = await JSON.stringify(products)
+            const productSave = await fs.writeFile(this.ruta, nuevaLista)
+            console.log(nuevaLista)
+            return obj.id
+        }else{  
+            obj.id = await 1;
+            archivos.push(obj)
+            const nuevaLista = await JSON.stringify(products)
+            const productSave = await fs.writeFile(this.ruta, nuevaLista)
+            console.log(nuevaLista)
+            return obj.id
+        }
         
+            
     }
-
+    
     async getById(id){
         const products = await this.getAll();
-        const productById = products.find(p=>p.id == id);
-        return productById;
+        let productById = await products.archivos.find(p => p.id == id)
+        console.log(productById)
+        return productById;  
     }
 
     async getAll(){
-        try{
-            const products = await FileSystem.readFile(this.ruta, 'utf-8');
-            return JSON.parse(products)
-        }catch(error){
-            return[];
-        }
+            const products = await fs.readFile(this.ruta, 'utf-8');
+            try {
+                const objJson = await JSON.parse(products);
+                return objJson
+            } catch (error) {
+                return {archivos:[]}
+            }
     }
 
-    async deleteById(id){
-        
+    async deleteById(idParam){
+        const products = await this.getAll();
+        const archivoos = await products.archivos
+        const productById = products.archivos.find(p => p.id == idParam)
+        const productFnal = productById.arrayposition
+        archivoos.splice(productFnal, 1)
+        const nuevaLista = await JSON.stringify(products)
+        await fs.writeFile(this.ruta, nuevaLista)
+        console.log(nuevaLista)
     }
 
     async deleteAll(){
-        
+        const products = await this.getAll();
+        let archivos = await products.archivos
+        await archivos.splice(0)
+        const nuevaLista = await JSON.stringify(products)
+        await fs.writeFile(this.ruta, nuevaLista)
+        console.log(nuevaLista)
     }
 }
+
 
 module.exports = Contenedor;
--- desafio02/test.js
+++ desafio02/test.js
@@ -1,15 +1,25 @@
-const Contenedor = require('./Contenedor');
+const Contenedor = require('./desafio02');
 
 async function main(){
+    
+    
+    const  ejemp01={
+        id:2,
+        texto: "descripcion texto",
+        numero : 1,
+}
+    
     const product = new Contenedor('./productos.txt');
-
-    console.log('Muestra todos los productos');
-    let allProducts = await product.getAll();
-    console.log(allProducts);
-
-    const idToSearch = 1;
-    console.log(`Muestro el producto con id ${idToSearch}`);
-    console.log(productById);
+    await product.getAll()
+    await product.save(ejemp01);
+    await product.save(ejemp01);
+    await product.getById(2)
+    await product.deleteById(1)
+    await product.getAll()
+    await product.deleteAll()
+   
 }
 
 main();
+
+
